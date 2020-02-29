1c39c7bc2eebe10c36f48796341e027d133e5828
williamdream <3339642010@qq.com>
2020-02-29 16:55:59 +0800

    init

---
+++ sell-product/src/main/java/com/william/sell/SellProductApp.java
@@ -0,0 +1,13 @@
+package com.william.sell;
+
+/**
+ * Hello world!
+ *
+ */
+public class SellProductApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ sell-product/src/test/java/com/william/sell/SellProductAppTest.java
@@ -0,0 +1,20 @@
+package com.william.sell;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple SellProductApp.
+ */
+public class SellProductAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
